67a23808a52251ff93c1d36892633335bb5c1834
Pritam Dodeja <pritam.dodeja@gmail.com>
2022-09-21 23:53:18 -0400

    Version 0.0.2 - Cleaned up some functions

--- src/tabular_knowledge/tabular_knowledge.py
+++ src/tabular_knowledge/tabular_knowledge.py
@@ -30,15 +30,9 @@ import scipy
 # ames_data.head()
 # pima_data.head()
 # # }}}
-# {{{ df_metadata function
-# start out with assuming all features are numerical
-# remove the ones that have object type from that list
-# from the remaining numerical, remove the ones that have less than threshold
-# different values they take on
-
-
+# {{{ df_metadata
 def df_metadata(df, numerical_threshold=50):
-    list_of_variables = list(df.dtypes.index)
+    list_of_variables = df.columns.tolist()
     list_of_dtypes = [df.dtypes[variable] for variable in list_of_variables]
     categorical_selector = selector(dtype_include=object)
     numerical_selector = selector(dtype_exclude=object)
@@ -103,9 +97,9 @@ def shorten_param(param_name):
 # Compute mutual information
 # Return mutual information
 
-def compute_mutual_information(df, target_label, random_state):
+def compute_mutual_information(df, target_label, meta_df, random_state, return_df=False, add_indicator=False, transform=True):
     # Analyze data frame
-    meta_df = df_metadata(df, numerical_threshold=50)
+    # meta_df = df_metadata(df, numerical_threshold=numerical_threshold)
     target_is_numerical = meta_df.loc[meta_df.variable == target_label][
         'is_numerical'].iloc[0]
 
@@ -118,10 +112,8 @@ def compute_mutual_information(df, target_label, random_state):
         mutual_information_function = mutual_info_classif
 
     # Select feature types
-    my_numerical_selector = feature_type_selector(dtype_include='numerical')
-    my_categorical_selector = feature_type_selector(dtype_include='categorical')
-    numerical_features = my_numerical_selector(df)
-    categorical_features = my_categorical_selector(df)
+    numerical_features = meta_df.loc[meta_df.is_numerical, 'variable'].tolist()
+    categorical_features = meta_df.loc[meta_df.is_numerical == False, 'variable'].tolist()
 
     # Remove target label from features
     for feature_list in [numerical_features, categorical_features]:
@@ -129,58 +121,64 @@ def compute_mutual_information(df, target_label, random_state):
             feature_list.remove(target_label)
 
     # Transform df
-    imputation_preprocessor = ColumnTransformer(
-        [('numerical_imputer',
-          SimpleImputer(strategy='median', add_indicator=True),
-          numerical_features),
-         ('categorical_imputer',
-          SimpleImputer(strategy='most_frequent', add_indicator=True),
-          categorical_features)],
-        remainder='passthrough')
+    if transform:
+        imputation_preprocessor = ColumnTransformer(
+            [('numerical_imputer',
+              SimpleImputer(strategy='median', add_indicator=add_indicator),
+              numerical_features),
+             ('categorical_imputer',
+              SimpleImputer(strategy='most_frequent', add_indicator=add_indicator),
+              categorical_features)],
+            remainder='passthrough')
 
-    # We need to figure out the indices to the features that are supposed to be scaled and encoded by the next
-    # step
+        # We need to figure out the indices to the features that are supposed to be scaled and encoded by the next
+        # step
 
-    post_imputation_np = imputation_preprocessor.fit_transform(df)
-    feature_name_np_array = imputation_preprocessor.get_feature_names_out()
-    categorical_feature_indices = np.zeros(len(categorical_features))
-    numerical_feature_indices = np.zeros(len(numerical_features))
+        post_imputation_np = imputation_preprocessor.fit_transform(df)
+        feature_name_np_array = imputation_preprocessor.get_feature_names_out()
+        categorical_feature_indices = np.zeros(len(categorical_features))
+        numerical_feature_indices = np.zeros(len(numerical_features))
 
-    for position, feature in enumerate(categorical_features):
-        categorical_feature_indices[position] = np.where(
-            feature_name_np_array == 'categorical_imputer__' + feature)[0]
+        for position, feature in enumerate(categorical_features):
+            categorical_feature_indices[position] = np.where(
+                feature_name_np_array == 'categorical_imputer__' + feature)[0]
 
-    for position, feature in enumerate(numerical_features):
-        numerical_feature_indices[position] = np.where(
-            feature_name_np_array == 'numerical_imputer__' + feature)[0]
+        for position, feature in enumerate(numerical_features):
+            numerical_feature_indices[position] = np.where(
+                feature_name_np_array == 'numerical_imputer__' + feature)[0]
 
-    categorical_feature_indices = categorical_feature_indices.astype(
-        int).tolist()
-    numerical_feature_indices = numerical_feature_indices.astype(int).tolist()
+        categorical_feature_indices = categorical_feature_indices.astype(
+            int).tolist()
+        numerical_feature_indices = numerical_feature_indices.astype(int).tolist()
 
-    numeric_and_categorical_transformer = ColumnTransformer(
-        [('OneHotEncoder', OneHotEncoder(),
-          categorical_feature_indices),
-         ('StandardScaler', StandardScaler(),
-          numerical_feature_indices)],
-        remainder='passthrough')
-    preprocessor = Pipeline(
-        [('imputation_preprocessor', imputation_preprocessor),
-         ('numeric_and_categorical_transformer',
-          numeric_and_categorical_transformer)])
-    df_transformed_np = preprocessor.fit_transform(df)
-    preprocessed_feature_names = list(preprocessor.get_feature_names_out())
-    if isinstance(df_transformed_np, scipy.sparse._csr.csr_matrix):
-        df_transformed = pd.DataFrame(
-            df_transformed_np.todense(),
-            columns=preprocessed_feature_names)
+        numeric_and_categorical_transformer = ColumnTransformer(
+            [('OneHotEncoder', OneHotEncoder(),
+              categorical_feature_indices),
+             # ('StandardScaler', StandardScaler(),
+             #  numerical_feature_indices)
+             ],
+            remainder='passthrough')
+        preprocessor = Pipeline(
+            [('imputation_preprocessor', imputation_preprocessor),
+             ('numeric_and_categorical_transformer',
+              numeric_and_categorical_transformer)])
+        df_transformed_np = preprocessor.fit_transform(df)
+        preprocessed_feature_names = list(preprocessor.get_feature_names_out())
+        if isinstance(df_transformed_np, scipy.sparse._csr.csr_matrix):
+            df_transformed = pd.DataFrame(
+                df_transformed_np.todense(),
+                columns=preprocessed_feature_names)
+        else:
+            df_transformed = pd.DataFrame(
+                df_transformed_np,
+                columns=preprocessed_feature_names)
+        df_transformed = df_transformed.rename(shorten_param, axis=1)
     else:
-        df_transformed = pd.DataFrame(
-            df_transformed_np,
-            columns=preprocessed_feature_names)
-    df_transformed = df_transformed.rename(shorten_param, axis=1)
+        df_transformed = df.copy()
+        preprocessed_feature_names = df.columns.tolist()
+    target_series = df[target_label]
     estimated_mutual_information = mutual_information_function(
-        X=df_transformed, y=df[target_label], random_state=random_state)
+        X=df_transformed, y=df[target_label].astype(int), random_state=random_state, n_neighbors=10)
     estimated_mutual_information_df = pd.DataFrame(
         estimated_mutual_information.T.reshape(
             1, -1), columns=preprocessed_feature_names)
@@ -189,10 +187,12 @@ def compute_mutual_information(df, target_label, random_state):
         axis=1)
     estimated_mutual_information_df = estimated_mutual_information_df.T
     estimated_mutual_information_df.columns = ['mutual_information']
-    estimated_mutual_information_df = estimated_mutual_information_df.sort_values(
-        by=['mutual_information'])
-
-    return estimated_mutual_information_df
+    # estimated_mutual_information_df = estimated_mutual_information_df.sort_values(
+    #     by=['mutual_information'])
+    if return_df:
+        return estimated_mutual_information_df, df_transformed
+    else:
+        return estimated_mutual_information_df
 # }}}
 # {{{ Selector based on mutual information
 
@@ -216,5 +216,21 @@ def mi_selector(mi_threshold=0.05, target_label=None, random_state=0):
         return matching_features
     return selector_to_return
 # }}}
+# {{{ get_df
+def get_df(pipe, data):
+    new_data = pipe.fit_transform(data)
+    if isinstance(new_data, scipy.sparse._csr.csr_matrix):
+        print(type(new_data))
+        return pd.DataFrame(
+            new_data.todense(),
+            columns=pipe.get_feature_names_out()).rename(
+            shorten_param, axis=1)
+    else:
+        return pd.DataFrame(
+            new_data,
+            columns=pipe.get_feature_names_out()).rename(
+            shorten_param,
+            axis=1)
+    # }}}
 if __name__ == '__main__':
     pass
